cfb76cdbb36851b597323b179ba01f0c67acdb1d
Atharv Sharma <atharv.sharma2912@gmail.com>
2024-10-31 16:27:22 +0530

    Nothing

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ class Commands:
             print("Acess Denied")
             exit()
             
+    def add(self):
+        add_1 = float(input("Enter the first number which you want to add: "))
+        add_2= float(input("Enter the second number you want to add:"))
+        print(f"The sum is {add_1+add_2}")
+        
     
