2c03d4ba6b0048761f211cff2ecf33e032e49433
Atharv Sharma <atharv.sharma2912@gmail.com>
2024-10-31 16:39:22 +0530

    A

--- main.py
+++ main.py
@@ -11,5 +11,29 @@ class Commands:
         add_1 = float(input("Enter the first number which you want to add: "))
         add_2= float(input("Enter the second number you want to add:"))
         print(f"The sum is {add_1+add_2}")
+    
+    def subtract(self):
         
+        sub_1 = float(input ("Please enter the first number : "))
+        sub_2 = float(input("Please ener the second number : "))
+        print(f"The difference is {sub_1-sub_2}:")
+        
+    def multiply(self):
+        
+        mul_1 = float(input ("Please enter the first number : "))
+        mul_2 = float(input("Please ener the second number : "))
+        print(f"The product is {mul_1*mul_2}")
+        
+    def divide(self):
+        div_1= float(input ("Please enter the first number : "))
+        div_2 = float(input("Please enter the second number : "))
+        try:
+            print(f"The qoutient is :{div_1/div_2}")
+            
+        except ZeroDivisionError:
+            print("OOPS THERE IS A ZERODIVISION ERROR")
+
+
+    
+
     
